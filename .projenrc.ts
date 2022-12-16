1900a7b5a9c97cd1efa2e07319c2eaaf39d5c9a3
Ryan Martin <ryan@vestahealthcare.com>
2022-12-16 17:48:56 -0500

    Additional projen config to publish to npm

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,11 +1,25 @@
 import { SkepPluginProject } from '@skeptools/skep-plugin-project';
+import { NpmAccess } from 'projen/lib/javascript';
+
+const majorVersion = 0;
 const project = new SkepPluginProject({
   cdktfProviderPackage: '@skeptools/provider-zenduty',
   defaultReleaseBranch: 'main',
   deps: ['timezone-enum@~1'],
   devDeps: ['@skeptools/skep-plugin-project'],
-  name: 'plugin-zenduty',
+  name: '@skeptools/plugin-zenduty',
   projenrcTs: true,
+  releaseToNpm: true,
+  npmAccess: NpmAccess.PUBLIC,
+  majorVersion,
+  releaseBranches: {
+    dev: { prerelease: 'dev', npmDistTag: 'dev', majorVersion },
+  },
+  depsUpgradeOptions: {
+    workflowOptions: {
+      branches: ['main'],
+    },
+  },
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
